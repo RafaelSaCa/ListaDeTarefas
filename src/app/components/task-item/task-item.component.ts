4990eacef34d2b63413e914454b6f6c9a49eccf8
Rafael Saca <raffaellsaca@gmail.com>
2024-08-20 18:24:15 -0300

    Componente de Apresentação TaskItem - exibe dados da tarefa e emite ação de deletar

---
+++ src/app/components/task-item/task-item.component.ts
@@ -0,0 +1,27 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { faTimesCircle } from '@fortawesome/free-regular-svg-icons';
+
+
+import { CommonModule } from '@angular/common';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { Tarefa } from '../../../../Tarefa';
+
+@Component({
+  selector: 'app-task-item',
+  standalone: true,
+  imports: [FontAwesomeModule,CommonModule],
+  templateUrl: './task-item.component.html',
+  styleUrl: './task-item.component.css'
+})
+export class TaskItemComponent {
+
+  @Input() tarefa! : Tarefa;
+  @Output() onDeleteTask = new EventEmitter<Tarefa>();
+  faTimes = faTimesCircle;
+
+  onDelete(tarefa: Tarefa) {
+    this.onDeleteTask.emit(tarefa);
+  }
+
+}
+
