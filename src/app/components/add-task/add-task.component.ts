01cb963e48414d69eed33cc695ac040c36db72fa
Rafael Saca <raffaellsaca@gmail.com>
2024-08-20 22:08:49 -0300

    Component Button que altera entre abrir ou fechar o formulario de tasks

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -1,40 +1,44 @@
-import { Tarefa } from './../../../../Tarefa';
 import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { ButtonComponent } from '../button/button.component';
+import { Tarefa } from './../../../../Tarefa';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-add-task',
   standalone: true,
-  imports: [FormsModule],
+  imports: [FormsModule, ButtonComponent, CommonModule],
   templateUrl: './add-task.component.html',
-  styleUrl: './add-task.component.css'
+  styleUrl: './add-task.component.css',
 })
 export class AddTaskComponent {
-
   @Output() onAddTask = new EventEmitter<Tarefa>();
 
   tarefa: string = '';
-  categoria: string='';
+  categoria: string = '';
   concluido: boolean = false;
+  mostrarAddTarefa: boolean = false;
+
+  alteraVisualizacao(valor: boolean) {
+    this.mostrarAddTarefa = valor;
+  }
 
   onSubmit() {
-    if (!this.tarefa){
-      alert('Adicione um tarefa')
+    if (!this.tarefa) {
+      alert('Adicione um tarefa');
       return;
     }
 
     const novaTarefa = {
       tarefa: this.tarefa,
       categoria: this.categoria,
-      concluido: this.concluido
-    }
+      concluido: this.concluido,
+    };
 
     this.onAddTask.emit(novaTarefa);
 
-    this.tarefa= '';
-    this.categoria ='';
+    this.tarefa = '';
+    this.categoria = '';
     this.concluido = false;
-
   }
-
 }
---
+++ src/app/components/button/button.component.ts
@@ -0,0 +1,21 @@
+import { CommonModule } from '@angular/common';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css'
+})
+export class ButtonComponent {
+
+  @Input() text: string ='';
+  @Input() color: string ='';
+  @Output() btnClick = new EventEmitter();
+
+
+  onClick() {
+  this.btnClick.emit();
+ }
+}
